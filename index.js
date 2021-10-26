c6080dfb4b7ac48a9068823c4b5b4906a2607da1
ibraim martinez <ibraimm5@gmail.com>
2021-10-25 23:42:20 -0400

    added array of questions for user input

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const path = require('path');
 const generateMarkdown = require('./utils/generateMarkdown')
 //TODO: const questions = [];
 // TODO: Create an array of questions for user input
@@ -9,8 +10,51 @@ const questions = [
     {
       type: 'input',
       name: 'title',
-      message: "What's the title",
+      message: "What's the title ?",
     },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email ?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'what is your Github username ?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Describe the application',
+    },
+    {
+      type: 'input',
+      name: 'license',
+      message: 'choose a license',
+      choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What should be the command to install dependecies ?',
+      default: 'npm i',
+    },
+    {
+      type: 'input', 
+      name: 'test',
+      message: 'What command should be used to run test ?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Any information for the user of the repository ?',
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'Any guidlines for contribution',
+    },
+
     
   ];
   inquirer.prompt(questions).then((answers) => {
